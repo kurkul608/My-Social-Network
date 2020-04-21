68244fda7d73156b7b811d0afde97d9e17f78a67
kurkul608 <molc.pyotr@yandex.ru>
2020-04-21 23:11:35 +0400

    pagination is modified

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import classes from './pagination.module.css'
 
 
-const Pagination = ({totalUsersCount, pageSize, currentPage, onPageChanged}) =>{
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+const Pagination = ({totalItemsCount, pageSize, currentPage, onPageChanged}) =>{
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
     let pages = [];
     pages.push(0);
@@ -15,10 +15,18 @@ const Pagination = ({totalUsersCount, pageSize, currentPage, onPageChanged}) =>{
         {pages.map(p => {
                 switch (p) {
                     case -1: return <></>
-                    case 0:  return <button key={p} onClick={(e) => {
+                    case 0:
+                        if (currentPage !== 1) { return (<span><button key={p} onClick={(e) => {
                         onPageChanged(p+1) }} className={classes.Page}>First </button>
-                    case pagesCount + 1: return <button  key={p}  onClick={(e) => {
-                        onPageChanged(p - 1)}} className={classes.Page}>Last</button>
+                        <button key={p} onClick={(e) => {
+                            onPageChanged(currentPage-1) }} className={classes.Page}>Prev </button></span>)}
+                            else return <></>
+                    case pagesCount + 1: 
+                    if (currentPage !== pagesCount) {return (<span><button  key={p}  onClick={(e) => {
+                        onPageChanged(currentPage + 1)}} className={classes.Page}>Next</button>
+                        <button  key={p}  onClick={(e) => {
+                        onPageChanged(p - 1)}} className={classes.Page}>Last</button></span>)}
+                        else return <></>
                     default: return <button key={p}   onClick={(e) => {
                         onPageChanged(p)}} className={currentPage === p && classes.selectedPage}> {p} </button>
                 }
